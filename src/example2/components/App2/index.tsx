8cff3cca9946389cc62d5b463e945061e6b65362
kinsei0916 <kinsei0916@gmail.com>
2020-10-08 18:56:16 +0900

    Remove comment

--- src/example2/components/App2/index.tsx
+++ src/example2/components/App2/index.tsx
@@ -3,9 +3,6 @@ import CountProvider from '../CountProvider'
 import Counter1 from './Counter1'
 import Counter2 from './Counter2'
 
-/**
- * Counter1を操作してもCounter2はレンダリングされない
- */
 const App1: React.FC = () => {
   console.log(`[${Date.now()}] App2 rendered`)
 
